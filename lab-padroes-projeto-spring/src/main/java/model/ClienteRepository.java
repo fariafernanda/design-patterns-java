74437e8b58c8466516afc9b43432bcd484666a28
Fernanda <fernandafaria.bio@gmail.com>
2022-05-31 17:10:03 -0300

    add: lab spring

---
+++ lab-padroes-projeto-spring/src/main/java/model/ClienteRepository.java
@@ -0,0 +1,9 @@
+package src.main.java.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
---
+++ lab-padroes-projeto-spring/src/main/java/service/ClienteService.java
@@ -0,0 +1,18 @@
+package src.main.java.service;
+
+import src.main.java.model.Cliente;
+
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
